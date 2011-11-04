04749af88d38b7b57296def6a8a6d876acfdb071
Chakkrit Sripariwut <chakkrit@abctech-thailand.com>
2011-11-04 17:25:57 +0700

    Add POD text to descript fizz_buzz.t and FizzBuzz.pm file into both file

--- lib/FizzBuzz.pm
+++ lib/FizzBuzz.pm
@@ -1,9 +1,30 @@
-#FizzBuzz.pm
-
 package FizzBuzz;
 
 use warnings;
 use strict;
+
+=head1 NAME
+
+FizzBuzz
+
+=head1 DESCRIPTION
+
+FizzBuzz is a package that implements the FizzBuzz to process number as input to return the result: 
+ 	"Fizz" 		when the input is a multiple number of three only, 
+	"Buzz" 		when the input is a multiple number of five only, 
+	"FizzBuzz"  	when the input is a multiple number of three and five,
+and	number		that is not pass the logics above.  
+
+=head1 METHODS
+
+=head2 fizz_buzz
+
+Description of fizz_buzz function
+
+It first takes input value to $number variable. Then it starts to process the logics.
+First rule, if the $number is multiple of three then the function must assign "Fizz" word to $text variable. Second rule, if the $number is multiple of Five then the function must assign "Buzz" word to $text variable. Third rule, if the $number is multiple of three and Five then the function must assign "FizzBuzz" word to $text variable. Last one,if the $number can not pass the all three logic above, then the function must assign the value of $number to $text variable. After they finish process the logics, the function will return the value of $text back.     
+
+=cut
 
 sub fizz_buzz {
   my ($number) = @_;
--- t/fizz_buzz.t
+++ t/fizz_buzz.t
@@ -4,11 +4,29 @@ use warnings;
 use strict;
 use Test::More tests => 5;
 
+=head1 NAME
+
+fizz_buzz
+
+=head1 DESCRIPTION
+
+fizz_buzz is testing unit file for FizzBuzz package. they are 5 testcases.
+
+1. Check for FizzBuzz.pm file exist and can be used by this testing unit
+
+2. Check for fizz_buzz function of FizzBuzz package take number that is multiple of Three then it function must return "Fizz" word
+
+3. Check for the function take number that is multiple of Five then it fuction must return "Buzz" word
+
+4. Check for the function take number that is multiple of Three and Five then it function must return "FizzBuzz" word
+
+5. Check for the function take number that is not multiple of three or Five the it function must return the number back 
+=cut
 
 use ok 'FizzBuzz';
-
 is(FizzBuzz::fizz_buzz(3), "Fizz", "the module gets 3 then it must returns Fizz");
 is(FizzBuzz::fizz_buzz(5), "Buzz", "the module gets 5 then it must returns Buzz");
 is(FizzBuzz::fizz_buzz(15), "FizzBuzz", "the module gets 15 then it must returns FizzBuzz");
 is(FizzBuzz::fizz_buzz(1), "1", "the module gets 1 then it must returns 1");
+
 
